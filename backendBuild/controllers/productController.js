c707e286ee69be3d12ae4152cced562d8293eb3f
D3lux3 <38981163+D3lux3@users.noreply.github.com>
2021-02-10 22:25:05 +0200

    Removed useless files

--- backendBuild/controllers/productController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paginateProductEntryData = void 0;
-const paginateProductEntryData = (data, page) => {
-    const pageParsed = page ? parseInt(page) : 0;
-    const start = 50 * pageParsed;
-    return data.slice(start, start + 50);
-};
-exports.paginateProductEntryData = paginateProductEntryData;
